dd19db6e3edf33812b7fac04c2a08d6afa695918
itsRoyMalka <115025973+itsRoyMalka@users.noreply.github.com>
2023-06-10 11:56:49 +0300

    Update Signout.jsx

--- src/pages/dashboard/signout/Signout.jsx
+++ src/pages/dashboard/signout/Signout.jsx
@@ -9,16 +9,20 @@ export const Signout = () => {
 
 
     const navigate = useNavigate()
-    const {setUser} = useContext(UserContext)
+    const {setUser,ready, setReady} = useContext(UserContext)
 
     const handleSignout = async () =>{
 
         await axios.post('/api/auth/signout')
             .then(res=>{
+                setReady(false)
                 setUser(null)
+
             })
             .catch(error=>{
+                console.log("here")
                 console.log(error)
+
 
             })
 
@@ -27,6 +31,7 @@ export const Signout = () => {
 
     useEffect(()=>{
         handleSignout().then(res=>{
+            console.log(ready)
             navigate('/login')
         })
     },[])
